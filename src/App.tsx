6f5f66e8dedb4b43af46f50d2a7ceb098bc1c76c
Rey Sajju <96593440+Reysajju@users.noreply.github.com>
2025-09-03 11:58:58 +0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import PrivacyPage from './pages/PrivacyPage';
 import ContactPage from './pages/ContactPage';
 import TermsPage from './pages/TermsPage';
 import RefundPolicyPage from './pages/RefundPolicyPage';
+import GhostwritingPage from './pages/services/GhostwritingPage';
+import EditingPage from './pages/services/EditingPage';
+import MarketingPage from './pages/services/MarketingPage';
+import PublishingPage from './pages/services/PublishingPage';
 import ServicePage from './pages/services/ServicePage';
 import ScrollToTop from './components/ScrollToTop';
 
@@ -21,11 +25,12 @@ function App() {
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/terms" element={<TermsPage />} />
           <Route path="/refund" element={<RefundPolicyPage />} />
-          <Route path="/services/editing" element={<ServicePage serviceName="Editing" />} />
+          <Route path="/services/ghostwriting" element={<GhostwritingPage />} />
+          <Route path="/services/editing" element={<EditingPage />} />
           <Route path="/services/fiction-writing" element={<ServicePage serviceName="Fiction Writing" />} />
-          <Route path="/services/marketing" element={<ServicePage serviceName="Marketing" />} />
+          <Route path="/services/marketing" element={<MarketingPage />} />
           <Route path="/services/non-fiction-writing" element={<ServicePage serviceName="Non Fiction Writing" />} />
-          <Route path="/services/publishing" element={<ServicePage serviceName="Publishing" />} />
+          <Route path="/services/publishing" element={<PublishingPage />} />
           <Route path="/services/story-book-writing" element={<ServicePage serviceName="Story Book Writing" />} />
           <Route path="/services/audio-book" element={<ServicePage serviceName="Audio Book" />} />
           <Route path="/services/biography-writing" element={<ServicePage serviceName="Biography Writing" />} />
